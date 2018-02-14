c23698c1c3f9f8c1134167525d1ad4af3ad65156
Łukasz Samul <samul.luk@gmail.com>
2018-02-14 20:37:03 +0100

    add first view

---
+++ WebContent/WEB-INF/views/index.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%@ include file="../header.jsp" %>
+	<h1>Aktualnie prowadzone naprawy</h1>
+	<table border=1>
+		<tr>
+			<th>Data przyjęcia do naprawy</th>
+			<th>Data rozpoczęcia do naprawy</th>
+			<th>Opis problemu</th>
+		</tr>
+		<c:forEach items="${orders}" var="order">
+		<tr>
+    		 <td>${order.accepted}</td>
+    		 <td>${order.begin}</td>
+    		 <td>${order.problem_description}</td>
+		</tr>
+ 		</c:forEach>
+	</table>
+	<%@ include file="../footer.jsp" %>
+</body>
+</html>
---
+++ src/pl/coderslab/controller/index.java
@@ -0,0 +1,62 @@
+package pl.coderslab.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.coderslab.dao.OrderDao;
+import pl.coderslab.model.Order;
+import pl.coderslab.service.DbUtil;
+
+/**
+ * Servlet implementation class index
+ */
+@WebServlet("/index")
+public class index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public index() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String status = "In repair";
+		try {
+			Connection c = DbUtil.getConn();
+			ArrayList<Order> currentOrders = OrderDao.loadAll(c);
+			request.setAttribute("orders", currentOrders);
+			
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		getServletContext().getRequestDispatcher("/WEB-INF/views/index.jsp")
+		.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
